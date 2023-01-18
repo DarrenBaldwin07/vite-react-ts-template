04492416662712abe546993dd22b4e7be641094e
Darren B <68653294+Devd0@users.noreply.github.com>
2023-01-18 14:42:21 -0500

    add types example

---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+// ----------- (THIS IS WHERE YOU WOULD PUT ALL APPLICATION TYPEs) -----------
+// Note: Ideally you eventually abstract into seperate files etc
+
+export interface Foo {
+    bar: string;
+}
